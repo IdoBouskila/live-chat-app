4b4fa81043be8fc111ac730831c7fdc493b05fc3
Ido Bouskila <idobouskila22@gmail.com>
2023-06-29 14:58:32 +0300

    refactor: save rooms images locally

--- client/src/components/RoomList.js
+++ client/src/components/RoomList.js
@@ -77,49 +77,49 @@ const rooms = [
     {
         id: 1,
         name: 'Dog Lovers 🐶',
-        src: 'https://www.willmarlakesarea.com/wp-content/uploads/2019/11/December-Blog-Dog-Friendly-Community-dog-park.jpg',
+        src: './rooms-images/dog-lovers.jpg',
         description: 'A community for dog lovers to share information, photos, experiences, and support each other.'
     },
 
     {
         id: 2,
         name: 'Developers 💻',
-        src: 'https://www.redlasso.com/wp-content/uploads/2020/03/Visual-Studio-Code-Vs-Community.jpg',
+        src: './rooms-images/developers.jpeg',
         description: 'Community for developers, we help each other.'
     },
     
     {
         id: 3,
         name: 'Foodies 🍕',
-        src: 'https://miro.medium.com/max/1400/0*PjchgA6hmUjQcF6g.png',
+        src: './rooms-images/foodies.png',
         description: 'A community of people who have a passion for food and love to explore new culinary experiences.'
     },
 
     {
         id: 4,
         name: 'Bookworms 📚',
-        src: 'https://bookwormreads.co/og_img.png',
+        src: './rooms-images/bookworms.png',
         description: 'Those who love to read and immerse themselves in books, and often discuss and share their favorite stories with others'
     },
 
     {
         id: 5,
         name: 'Movie Buffs 🎬',
-        src: 'https://www.parkgrandkensington.co.uk/blog/wp-content/uploads/2019/10/watching-cinema-1280x720.jpg',
+        src: './rooms-images/movie-buffs.jpg',
         description: 'A group of individuals who love to engage in outdoor activities such as hiking, camping, and rock climbing'
     },
 
     {
         id: 6,
         name: 'DIYers 🙌',
-        src: 'https://d9e8c7w7.stackpathcdn.com/wp-content/uploads/2020/04/DIY-During-Lockdown.jpg',
+        src: './rooms-images/diyers.jpg',
         description: ' People who like to take on home improvement and craft projects, and enjoy working with their hands to create something new and unique'
     },
 
     {
         id: 7,
         name: 'Fitness Enthusiasts 💪🏽',
-        src: 'https://assets.website-files.com/581c85345d7e0501760aa7db/5b17ab5cc6215ef0331908fd_Creative%20Ways%20to%20Build%20Community%20at%20Your%20Gym.jpg',
+        src: './rooms-images/fitness.jpg',
         description: 'Individuals who have a love for films and enjoy watching and discussing different genres, styles, and storylines.'
     }
 ];
